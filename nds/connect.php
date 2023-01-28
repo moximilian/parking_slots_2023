711fba28023e56b85424e5103c3465c6b4fc303e
Maxim <63146466+moximilian@users.noreply.github.com>
2023-01-28 16:01:22 +0400

    Add files via upload
    Подключение к фит мосполитех

--- nds/connect.php
+++ nds/connect.php
@@ -1,3 +1,5 @@
 <?php 
-$connect = mysqli_connect('127.0.0.1','root','root','parkings');
+$connect = mysqli_connect('std-mysql.ist.mospolytech.ru:3306', 'std_2101_parkings2023', '123123123', 'std_2101_parkings2023');
+//$connect = mysqli_connect('localhost', 'root', 'root', 'parkings');
+if($connect->connect_errno) exit('Ошибка подключения к БД');
 ?>
